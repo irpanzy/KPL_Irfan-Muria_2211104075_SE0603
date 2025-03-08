abb697f3dc0fa2e76db80a75a71debd724f806d3
pasipasipada <irfanoputra27@gmail.com>
2025-03-08 12:26:08 +0700

    feat: add variable.js

---
+++ 02_Pengenalan_IDE_dan_Pemrograman_NodeJs/variable.js
@@ -0,0 +1,15 @@
+var name = "Irfan Muria";
+const nim = 2211104075;
+
+console.log(name);
+console.log(nim);
+
+console.log("nama saya adalah " + name + " dengan NIM " + nim);
+console.log(`nama saya adalah ${name} dengan NIM ${nim}`);
+
+console.log(`Nama saya adalah ${name}
+    nim saya adalah ${nim}`);
+
+var name = "irpan";
+
+let fullName = "Irfan Muria";
