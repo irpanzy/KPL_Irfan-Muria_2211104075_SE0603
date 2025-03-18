c517e1f2793d7bf42f038539929b5be8c416142c
pasipasipada <irfanoputra27@gmail.com>
2025-03-18 19:54:16 +0700

    feat: add class posisi karakter game

--- 04_Automata_dan_Table-Driven_Construction/Jurnal/index.js
+++ 04_Automata_dan_Table-Driven_Construction/Jurnal/index.js
@@ -1,5 +1,12 @@
 const KodeBuah = require("./kodeBuah");
+const PosisiKarakterGame = require("./posisiKarakterGame");
 
 const kodeBuah = new KodeBuah();
 console.log("Kode Buah Apel: " + kodeBuah.getKodeBuah("Apel"));
 console.log("Kode Buah Pisang: " + kodeBuah.getKodeBuah("Pisang"));
+
+const posisiKarakter = new PosisiKarakterGame(123456);
+posisiKarakter.simulateKeyPress("S");
+posisiKarakter.simulateKeyPress("W");
+
+console.log("Posisi karakter: " + posisiKarakter.getState());
