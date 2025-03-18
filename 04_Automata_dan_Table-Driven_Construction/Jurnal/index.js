7af49cb2f5917c0dca057518e75ec9fdd640a311
pasipasipada <irfanoputra27@gmail.com>
2025-03-18 19:36:14 +0700

    feat: add class kode buah

---
+++ 04_Automata_dan_Table-Driven_Construction/Jurnal/index.js
@@ -0,0 +1,5 @@
+const KodeBuah = require("./kodeBuah");
+
+const kodeBuah = new KodeBuah();
+console.log("Kode Buah Apel: " + kodeBuah.getKodeBuah("Apel"));
+console.log("Kode Buah Pisang: " + kodeBuah.getKodeBuah("Pisang"));
